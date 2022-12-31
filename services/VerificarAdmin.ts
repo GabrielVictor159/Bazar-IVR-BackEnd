5a7c5f27d5d2b061d558745bfbb4653dc7c79472
GabrielVictor159 <gabrielvictor159487@gmail.com>
2022-12-31 06:30:04 -0300

    alterando as funções de admin

--- services/VerificarAdmin.ts
+++ services/VerificarAdmin.ts
@@ -1,9 +1,21 @@
-import fs from 'fs';
-
-export default function VerificarAdmin(nome: string, senha: string): boolean {
-  const data = fs.readFileSync('../services/admin.json', 'utf8');
-
-        // Converte o conteúdo do arquivo para um objeto JavaScript
-  let admin = JSON.parse(data);
-  return admin.some((a: { NomeAdmin: string; SenhaAdmin: string; }) => a.NomeAdmin === nome && a.SenhaAdmin === senha);
+import Admin from "../models/Admin";
+var md5 = require('md5');
+export default  function VerificarAdmin(NomeAdmin:String, SenhaAdmin:String){
+    try{
+    const buscaAdmin =  Admin.findOne({
+        where:{
+            Nome:NomeAdmin,
+            Senha:md5(SenhaAdmin)
+        }
+    })
+    if(buscaAdmin==null){
+        return false
+    }
+    else{
+        return true
+    }
+}
+catch(exception:any){
+    console.log(exception.message)
+}
 }
