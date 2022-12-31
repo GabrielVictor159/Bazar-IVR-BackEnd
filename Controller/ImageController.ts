f097eb25b19ac662851d3db41631e076b3998054
GabrielVictor159 <gabrielvictor159487@gmail.com>
2022-12-31 03:32:09 -0300

    arrumando confirmação de usuarios

--- Controller/ImageController.ts
+++ Controller/ImageController.ts
@@ -1,34 +1,14 @@
-import express from 'express';
-import { json } from 'sequelize';
-import { ImageService } from '../services/ImageService';
-import VerificarAdmin from '../services/VerificarAdmin';
+import { Router, Request, Response } from 'express';
+import ImageService from '../services/ImageService';
 
-const imageService = new ImageService();
-const ImageController = express.Router();
+const router = Router();
 
-ImageController.post('/images/:NomeAdmin/:SenhaAdmin', async(req, res, next) => {
-console.log(req)
-if(await VerificarAdmin(req.params.NomeAdmin, req.params.SenhaAdmin)){
-const resposta = await imageService.saveImage(req)
-res.send("sucesso")
-res.end();
-}
-else{
-res.send("Senha de Administrador errada")
-res.end();
-}
+router.post('/', (req: Request, res: Response) => {
+ImageService.saveImage(req, res);
 });
 
-ImageController.delete('/images/:name', async (req, res)=>{
-const resposta = imageService.deleteImage(req.params.name)
-if( await resposta===false){
-res.send("houve um erro")
-res.end()
-}
-else{
-res.send(resposta)
-res.end();
-}
+router.delete('/:name', (req: Request, res: Response) => {
+ImageService.deleteImage(req, res);
 });
 
-export default ImageController;
+export default router;
