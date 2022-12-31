74bc69ef24ed42fbab5320be4d824a2182e7f2e8
GabrielVictor159 <gabrielvictor159487@gmail.com>
2022-12-31 03:14:33 -0300

    arrumando confirmação de usuarios

--- Controller/ImageController.ts
+++ Controller/ImageController.ts
@@ -2,31 +2,33 @@ import express from 'express';
 import { json } from 'sequelize';
 import { ImageService } from '../services/ImageService';
 import VerificarAdmin from '../services/VerificarAdmin';
+
 const imageService = new ImageService();
 const ImageController = express.Router();
 
 ImageController.post('/images/:NomeAdmin/:SenhaAdmin', async(req, res, next) => {
-  console.log(req)
-   if(await VerificarAdmin(req.params.NomeAdmin, req.params.SenhaAdmin)){
-     const resposta =  imageService.saveImage(req)
+console.log(req)
+if(await VerificarAdmin(req.params.NomeAdmin, req.params.SenhaAdmin)){
+const resposta = await imageService.saveImage(req)
+res.send("sucesso")
+res.end();
+}
+else{
+res.send("Senha de Administrador errada")
+res.end();
+}
+});
 
-     res.send("sucesso")
-     res.end();
-   }
-   else{
-    res.send("Senha de Administrador errada")
-     res.end();
-   }
-  });
-  ImageController.delete('/images/:name', (req, res)=>{
-    const resposta = imageService.deleteImage(req.params.name)
-    if( resposta===false){
-        res.send("houve um erro")
-        res.end()
-    }
-    else{
-        res.send(resposta)
-        res.end();
-    }
-  })
-export default ImageController
+ImageController.delete('/images/:name', async (req, res)=>{
+const resposta = imageService.deleteImage(req.params.name)
+if( await resposta===false){
+res.send("houve um erro")
+res.end()
+}
+else{
+res.send(resposta)
+res.end();
+}
+});
+
+export default ImageController;
