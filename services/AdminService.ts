57da44e1a919cc318be76523a2af79ac46390f27
GabrielVictor159 <gabrielvictor159487@gmail.com>
2022-12-31 06:34:02 -0300

    alterando as funções de admin

--- services/AdminService.ts
+++ services/AdminService.ts
@@ -5,7 +5,7 @@ import VerificarAdmin from './VerificarAdmin';
 export class AdminService {
     constructor() { }
 
-    Login = (Nome: string, Senha: string) => {
+    Login = (Nome: String, Senha: String) => {
       const resposta =  VerificarAdmin(Nome, Senha)
       return resposta
 
