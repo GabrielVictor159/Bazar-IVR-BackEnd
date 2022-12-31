35813304a6deb3f614e13903b610ddca5c41ee91
GabrielVictor159 <gabrielvictor159487@gmail.com>
2022-12-31 06:06:18 -0300

    alterando as funções de admin

---
+++ build/Admin.js
@@ -0,0 +1,12 @@
+export interface Admin {
+    NomeAdmin: string;
+    SenhaAdmin: string;
+  }
+  
+  export const admin: Admin[] = [
+    {
+      NomeAdmin: "gabriel",
+      SenhaAdmin: "klo9sk7p3"
+    }
+  ];
+  
---
+++ build/services/Admin.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.admin = void 0;
+exports.admin = [
+    {
+        NomeAdmin: "gabriel",
+        SenhaAdmin: "klo9sk7p3"
+    }
+];
+//# sourceMappingURL=Admin.js.map
